3382c3137e584cdc5420efca2718616ffae29d62
suyogpunde0411 <suyogpunde0411@gmail.com>
2026-05-08 12:43:35 +0530

    improvement

--- server/server.js
+++ server/server.js
@@ -7,10 +7,6 @@ const taskRoutes = require('./routes/taskRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -22,7 +18,7 @@ app.use('/api/tasks', taskRoutes);
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log('Connected to MongoDB Atlas');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 })
 .catch(err => {
     console.error('MongoDB connection error:', err);
